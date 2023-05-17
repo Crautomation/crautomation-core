b74fe2437d0892a2470a301076a02e6909ced040
Craig Brooks <craig-brooks@hotmail.co.uk>
2023-05-17 19:20:35 +0100

    Removed listener whilst resolving issue

--- src/main/java/com/github/crautomation/core/common/listeners/LoggerListener.java
+++ src/main/java/com/github/crautomation/core/common/listeners/LoggerListener.java
@@ -1,55 +1,55 @@
-package com.github.crautomation.core.common.listeners;
-
-import com.github.crautomation.core.common.properties.constants.CustomResources;
-import com.github.crautomation.core.common.test.BaseTest;
-import com.github.crautomation.core.common.util.ProminentStep;
-import io.qameta.allure.Attachment;
-import org.testng.ITestResult;
-import org.testng.TestListenerAdapter;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-
-/**
- * LoggerListener
- * <p>
- * Handles attaching the test specific log files to the Tests for reporting.
- */
-public class LoggerListener extends TestListenerAdapter {
-    @Override
-    public synchronized void onTestSkipped(final ITestResult testResult) {
-        attachLogFile(testResult);
-    }
-
-    @Override
-    public synchronized void onTestFailure(final ITestResult testResult) {
-        attachLogFile(testResult);
-    }
-
-    @Override
-    public synchronized void onTestSuccess(final ITestResult testResult) {
-        attachLogFile(testResult);
-    }
-
-    @Attachment(value = "Log File")
-    private String attachLogFile(final ITestResult tr) {
-        ProminentStep.create("Attaching Log File to report.");
-
-        final Object currentClass = tr.getInstance();
-        final String logFileName = ((BaseTest) currentClass).getLoggerFileName();
-
-        try {
-            return new String(Files.readAllBytes(new File(CustomResources.LOG_LOCATION.concat(logFileName).concat(".log")).toPath()));
-        } catch (FileNotFoundException fnf) {
-            assertThat("Unable to find log file, is your -Dproject.builddir set correctly?", false);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return "There was an error attaching the log file to this report.";
-    }
-}
+//package com.github.crautomation.core.common.listeners;
+//
+//import com.github.crautomation.core.common.properties.constants.CustomResources;
+//import com.github.crautomation.core.common.test.BaseTest;
+//import com.github.crautomation.core.common.util.ProminentStep;
+//import io.qameta.allure.Attachment;
+//import org.testng.ITestResult;
+//import org.testng.TestListenerAdapter;
+//
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
+//import java.nio.file.Files;
+//
+//import static org.hamcrest.MatcherAssert.assertThat;
+//
+///**
+// * LoggerListener
+// * <p>
+// * Handles attaching the test specific log files to the Tests for reporting.
+// */
+//public class LoggerListener extends TestListenerAdapter {
+//    @Override
+//    public synchronized void onTestSkipped(final ITestResult testResult) {
+//        attachLogFile(testResult);
+//    }
+//
+//    @Override
+//    public synchronized void onTestFailure(final ITestResult testResult) {
+//        attachLogFile(testResult);
+//    }
+//
+//    @Override
+//    public synchronized void onTestSuccess(final ITestResult testResult) {
+//        attachLogFile(testResult);
+//    }
+//
+//    @Attachment(value = "Log File")
+//    private String attachLogFile(final ITestResult tr) {
+//        ProminentStep.create("Attaching Log File to report.");
+//
+//        final Object currentClass = tr.getInstance();
+//        final String logFileName = ((BaseTest) currentClass).getLoggerFileName();
+//
+//        try {
+//            return new String(Files.readAllBytes(new File(CustomResources.LOG_LOCATION.concat(logFileName).concat(".log")).toPath()));
+//        } catch (FileNotFoundException fnf) {
+//            assertThat("Unable to find log file, is your -Dproject.builddir set correctly?", false);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return "There was an error attaching the log file to this report.";
+//    }
+//}
