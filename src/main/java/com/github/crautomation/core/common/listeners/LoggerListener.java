f86e52c0d94311107543b6feb7452ebf5caafafa
Craig Brooks <craig-brooks@hotmail.co.uk>
2023-05-17 19:32:34 +0100

    Version bump

--- src/main/java/com/github/crautomation/core/common/listeners/LoggerListener.java
+++ src/main/java/com/github/crautomation/core/common/listeners/LoggerListener.java
@@ -1,4 +1,5 @@
 package com.github.crautomation.core.common.listeners;
+
 //
 //import com.github.crautomation.core.common.properties.constants.CustomResources;
 //import com.github.crautomation.core.common.test.BaseTest;
